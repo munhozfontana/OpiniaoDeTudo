077cddb0a0060c9a3e1301402b46f47215564929
luís fernando <munhozfontana@gmail.com>
2019-11-13 19:14:50 -0300

    implementação do menu inferior

--- app/src/main/java/com/example/opiniaodetudo/pages/MainActivity.kt
+++ app/src/main/java/com/example/opiniaodetudo/pages/MainActivity.kt
@@ -11,25 +11,34 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
 import com.example.opiniaodetudo.R
 import com.example.opiniaodetudo.domain.Review
 import com.example.opiniaodetudo.infra.repositories.ReviewRepository
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val fragments = mapOf(FORM_FRAGMENT to ::FormFragment, LIST_FRAGMENT to ::ListFragment)
+
+    companion object {
+        val FORM_FRAGMENT = "formFragment"
+        val LIST_FRAGMENT = "listFragment"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container_main, FormFragment())
-            .commit()
+
         configureAutoHiddenKeyboard()
+        navigateTo(FORM_FRAGMENT)
+        configureBottomMenu()
     }
 
 
-    private fun configureAutoHiddenKeyboard(){
+    private fun configureAutoHiddenKeyboard() {
         val mainContainer = findViewById<ConstraintLayout>(R.id.main_container)
-        mainContainer.setOnTouchListener { _ , _ ->
+        mainContainer.setOnTouchListener { _, _ ->
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
         }
@@ -40,6 +49,23 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         finish()
         return true
+    }
+
+    fun navigateTo(item: String) {
+        val fragmentInstance: Fragment = fragments[item]?.invoke()!!
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container_main, fragmentInstance)
+            .addToBackStack(item).commit()
+    }
+
+    private fun configureBottomMenu() {
+        val bottomNavigationMenu = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationMenu.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menuitem_newitem -> navigateTo(FORM_FRAGMENT)
+                R.id.menuitem_listitem -> navigateTo(LIST_FRAGMENT)
+            }
+            true
+        }
     }
 
 
